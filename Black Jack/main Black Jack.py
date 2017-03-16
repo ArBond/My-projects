955cec241bd57dc8260878fe757231bda69e572c
ArBond <bonartembon@gmail.com>
2017-03-16 11:48:00 +0300

    Update main Black Jack.py

--- Black Jack/main Black Jack.py
+++ Black Jack/main Black Jack.py
@@ -1,8 +1,30 @@
 import pygame
+import random
 import sys
 
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Black Jack")
+
+deskSize = 52
+cardDesk = []
+suit = ['D', 'H', 'S', 'C']
+suitNumber = 0
+cardNumber = 2
+for i in range(deskSize):
+    cardDesk.append([cardNumber])
+    cardDesk[i].append(suit[suitNumber])
+    cardNumber += 1
+    if cardNumber == 15:
+        suitNumber += 1
+        cardNumber = 2
+
+cardDesk *= 3
+deskSize *= 3
+
+random.shuffle(cardDesk)
+
+for i in range(deskSize):
+    print(i + 1, cardDesk[i])
 
 gameScreen = pygame.Surface((800, 600))
 
